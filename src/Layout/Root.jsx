300bf629623a801c49520eff76400bb38f0960d1
hamed18 <hamedcnec@gmail.com>
2024-08-05 01:07:30 +0600

    Navbar using Daisy UI

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,12 +1,8 @@
 import { Outlet } from "react-router-dom";
-import Header from "../Shared/Header";
-import Navbar from "../Shared/Navbar";
 
 const Root = () => {
 	return (
 		<div className="max-w-6xl mx-auto font-Poppins">
-		    <Header></Header>
-			<Navbar></Navbar>
 			<Outlet></Outlet>
 		</div>
 	);
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,11 @@
+import Header from "../../Shared/Header";
+import Navbar from "../../Shared/Navbar";
 
 const Home = () => {
 	return (
 		<div className="text-center">
+			<Header></Header>
+			<Navbar></Navbar>
 			<p className="text-center font-Poppins">This is Home Page</p>
 		</div>
 	);
