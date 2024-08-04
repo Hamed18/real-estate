56536963bbd67fcfcb158a593e78a7d13356966a
hamed18 <hamedcnec@gmail.com>
2024-08-05 02:01:39 +0600

    created a json file of estates data

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,11 @@
+import { useLoaderData } from "react-router-dom";
 import Footer from "../../Shared/Footer";
 import Header from "../../Shared/Header";
 import Navbar from "../../Shared/Navbar";
 
 const Home = () => {
+	const estates = useLoaderData();
+	console.log(estates);
 	return (
 		<div className="text-center">
 			<Navbar></Navbar>
