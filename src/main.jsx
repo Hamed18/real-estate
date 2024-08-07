038bb1db9c8092d51822bda7ad30453207466c63
hamed18 <hamedcnec@gmail.com>
2024-08-08 02:11:36 +0600

    Create context API and used in Register page

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import './index.css'
 import routes from './Route/Route'
+import AuthProviders from './Providers/AuthProviders'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={routes}></RouterProvider>
+    <AuthProviders>
+       <RouterProvider router={routes}></RouterProvider>
+    </AuthProviders>
   </React.StrictMode>,
 )
